da076c2fa31bfdbf5d0e6acb41d8b89c0157a1fd
aditi110698 <80385577+aditi110698@users.noreply.github.com>
2024-05-17 13:38:21 +0530

    Delete Cryptography directory

--- Cryptography/Symmetric_encryption/test_ARC4.py
+++
@@ -1,8 +0,0 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-
-algorithm = algorithms.ARC4(key)
-cipher = Cipher(algorithm, mode=None)
-encryptor = cipher.encryptor()
-ct = encryptor.update(b"a secret message")
-decryptor = cipher.decryptor()
-decryptor.update(ct)
